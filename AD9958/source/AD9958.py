d1695a61533fd602bf58b66663b602a5e2f8ca53
DSPsandbox <54580162+dspsandbox@users.noreply.github.com>
2019-10-05 20:38:58 +0200

    Update AD9958.py

--- AD9958/source/AD9958.py
+++ AD9958/source/AD9958.py
@@ -8,7 +8,7 @@
 #
 #-----------------------------------------------------------------------------
 # MIT License
-# Copyright (c) 2018 Pau Gomez Kabelka <paugomezkabelka@gmail.com>
+# Copyright (c) 2019 DSPsandbox (pau.gomez@dspsandbox)
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
